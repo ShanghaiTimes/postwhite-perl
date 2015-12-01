8e5bf26858418e92bdf2aeecf30eb79b5f9f8139
ShanghaiTimes <racuk12@gmail.com>
2015-12-01 13:54:58 +0000

    Updating to GitHub
    Updating to GitHub

---
+++ README.md
@@ -0,0 +1,12 @@
+Taking the most excellent script by Steve Jenkins and added my perlsort to it
+Just because really ...
+Perl is fast, and just about anyone doing any sort of System Admin will be familiar with it, and have it. So it's not a drama.
+
+Tue  1 Dec 2015 13:47:15 GMT
+
+Robert Chalmers
+robert@chalmers.com.au Coding and such like - pretty much OSX specific
+robert@chalmers.com.au  Quantum Radio: http://tinyurl.com/lwwddov
+Mac mini 6.2 - 2012, Intel Core i7,2.3 GHz, Memory:16 GB. El-Capitan 10.11. 2TB Storage made up of - 
+Drive 0:HGST HTS721010A9E630. Upper bay. Drive 1:ST1000LM024 HN-M101MBB. Lower Bay
+
---
+++ perlsort.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+
+#/ perlsort-1.pl
+#/ Robert Chalmers
+#/ robert@chalmers.com.au
+#/ Twitter: @ShanghaiTimes
+#/ 29th November 2015
+#/ Sort a huge list of IP addresses, both IPv4 and IPv6
+#/ originally produced by Steve Jenkin's great work, 'postwhite'
+#/ See the comments sprinkled throughout...
+
+# Some CPAN includes - should be built into most
+use strict;
+use Path::Class;
+use Net::IP;
+use List::MoreUtils qw(uniq);
+
+use vars qw/@ip_strings/;
+
+my ($src, $dest) = @ARGV;
+
+#my $dir = dir("/tmp"); # /tmp
+#my $file = $dir->file("outfile.txt"); # /tmp/file.txt
+
+
+my $file = $ENV{TMP5};
+
+
+my $string = '     permit';
+#my $filename = 'unsorted.txt';
+my $filename = $ENV{TMP3};
+
+
+#or $src=$ENV{TRMP3};
+#   $dest=$ENV{5};
+
+
+# Open the output file ready for writing.
+open(my $fh, '>', $file) or die "Could not open file '$file' $!";
+# Get a file_handle (IO::File object) you can write to
+#my $file_handle = $file->openw();
+
+# Open the input file. Store it's contents into @ip_strings
+open (FH, "< $filename") or die "Can't open $filename for read: $!";
+my @ip_strings = <FH>;
+close FH or die "Cannot close $filename: $!";
+
+# Strip the word "permit" from the end so sort can do it's thing
+        foreach my $line (@ip_strings) { 
+	$line =~ s/\s+\w+$//;
+                };
+# Sort the whole thing...
+my @sorted = 
+    map  { $_->[0] }
+    sort { $a->[1] <=> $b->[1] }
+    map  { [ $_, eval { Net::IP->new( $_ )->intip } ] }
+    @ip_strings;
+
+	# use unique to sort the unique words from @sorted.
+	my @unique_words = uniq @sorted;
+
+	# chomp the newline from the end of the string, then add 'permit' again
+       	 foreach my $line (@unique_words) {
+	 chomp $line;
+	 my $newline = "${line}${string}\n";
+	# print it out to the 'outfile.txt'
+	 print $fh $newline;
+                };	
+# Close it up and go home...	
+close $fh;
